2d15107411ed47ecf231d5f032adac7f6d7aa175
Valkryst <github@valkryst.com>
2023-11-24 12:53:45 -0400

    Add `relabelKeypoint` to `FaceDetector` Fix issue where `FaceDetector` could pass an undefined value to `Mesh#updateFaceKeypoints`

--- js/face_detector.js
+++ js/face_detector.js
@@ -1,4 +1,4 @@
-import {validateInstanceOf, validateNumber} from "./validation.js";
+import {validateDefined, validateInstanceOf, validateNumber} from "./validation.js";
 
 /*
  * See the following link for more information about the face landmarks detection model:
@@ -51,7 +51,15 @@ export class FaceDetector {
             const currentTime = performance.now();
 
             const rawFaces = await this.detector.estimateFaces(videoElement);
+            if (rawFaces.length === 0) {
+                return;
+            }
+
             const rawFace = rawFaces[0];
+            for (let i = 0 ; i < rawFace.keypoints.length ; i++) {
+                this.relabelKeypoint(i, rawFace.keypoints[i]);
+            }
+
             mesh.updateFaceKeypoints(rawFace);
 
             this.lastRuntime = performance.now() - currentTime;
@@ -64,6 +72,52 @@ export class FaceDetector {
             clearInterval(this.intervalId);
             this.intervalId = null;
         }
+    }
+
+    /**
+     * Relabels a raw Keypoint object, if necessary.
+     *
+     * Some calculations require specific points which have generic names. To get around this, we rename
+     * these points to something more specific.
+     *
+     * The array of points is, as far as I can tell, always in the same order. This means that we can
+     * reference the following mesh diagram to find specific points and rename them.
+     *
+     * https://github.com/tensorflow/tfjs-models/blob/master/face-landmarks-detection/mesh_map.jpg
+     *
+     * @param {number} index Index of the Keypoint in the array of Keypoints.
+     * @param {object} rawKeypoint Raw Keypoint object to relabel.
+     */
+    relabelKeypoint(index, rawKeypoint) {
+        validateDefined(rawKeypoint);
+
+        let name = rawKeypoint.name;
+
+        switch (index) {
+            case 10: {
+                name = "top_edge_face";
+                break;
+            }
+            case 152: {
+                name = "bottom_edge_face";
+                break;
+            }
+            case 164: {
+                console.log("Detected midpoint between nose and mouth.")
+                name = "midpoint_between_nose_and_mouth";
+                break;
+            }
+            case 234: {
+                name = "right_edge_face";
+                break;
+            }
+            case 454: {
+                name = "left_edge_face";
+                break;
+            }
+        }
+
+        rawKeypoint.name = name;
     }
 
     /**
