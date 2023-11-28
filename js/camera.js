0a87eb10cc2b50bb3e20f49a24ddb0c20bfbd8cc
Valkryst <github@valkryst.com>
2023-11-28 15:57:57 -0400

    Update `Camera` to memoize the `video` element

--- js/camera.js
+++ js/camera.js
@@ -8,6 +8,8 @@ export class Camera {
      */
     constructor(deviceId) {
         this.deviceId = deviceId;
+
+        this.videoElement = null;
     }
 
     /**
@@ -88,35 +90,23 @@ export class Camera {
     }
 
     /**
-     * Creates or updates a video element, so that it displays the video input feed from the device associated with
-     * this Camera object.
+     * Updates the `height`, `srcObject`, and `width` properties of the video element associated with this Camera.
      *
-     * @param {HTMLVideoElement|null} video The video element to display the video input feed. If unspecified, a new video element will be created.
-     *
-     * @throws {Error} If there is an issue retrieving the video input device.
-     *
-     * @returns {Promise<HTMLVideoElement>} A promise that resolves to a video element.
+     * @returns {Promise<void>} A promise that resolves when the video element has been updated.
      */
-    async createOrUpdateVideoElement(video) {
-        try {
-            validateInstanceOf(video, HTMLVideoElement);
-        } catch(e) {
-            video = document.createElement("video");
+    async updateVideoElement() {
+        if (this.videoElement == null) {
+            return;
         }
 
-        video.autoplay = true;
-        video.muted = true;
-        video.playsinline = true;
-        video.srcObject = await this.getVideoInputDevice();
+        this.videoElement.srcObject = await this.getVideoInputDevice();
 
         /*
          * Both FaceDetector and HandDetector use the video element's height and width properties to scale the input
          * MediaStream before processing it. This is why we need to set them.
          */
-        video.height = video.clientWidth / (await this.getMediaStreamAspectRatio());
-        video.width = video.clientWidth;
-
-        return video;
+        this.videoElement.height = this.videoElement.clientWidth / (await this.getMediaStreamAspectRatio());
+        this.videoElement.width = this.videoElement.clientWidth;
     }
 
     /**
@@ -163,6 +153,29 @@ export class Camera {
     }
 
     /**
+     * Retrieves the video element associated with this Camera object, creating it if necessary.
+     *
+     * @throws {Error} If there is an issue retrieving the video input device.
+     *
+     * @returns {Promise<HTMLVideoElement>} A promise that resolves to a video element.
+     */
+    async getVideoElement() {
+        if (this.videoElement != null) {
+            await this.updateVideoElement();
+            return this.videoElement;
+        }
+
+        const video = document.createElement("video");
+        video.autoplay = true;
+        video.muted = true;
+        video.playsinline = true;
+        this.videoElement = video;
+
+        await this.updateVideoElement();
+        return video;
+    }
+
+    /**
      * Attempts to retrieve the video input device associated with this Camera object.
      *
      * See https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia for a list of all exceptions.
@@ -189,5 +202,16 @@ export class Camera {
     static async getVideoInputDevices() {
         const devices = await navigator.mediaDevices.enumerateDevices();
         return devices.filter(device => device.kind === "videoinput");
+    }
+
+    /**
+     * Sets the video element associated with this Camera object.
+     *
+     * @param {HTMLVideoElement} videoElement The new video element to use.
+     */
+    async setVideoElement(videoElement) {
+        validateInstanceOf(videoElement, HTMLVideoElement);
+        this.videoElement = videoElement;
+        await this.updateVideoElement();
     }
 }
